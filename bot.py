19e9a5495ce6329af338b6e1273a28bdd3a87c00
Daniel Einor <deino@softserveinc.com>
2021-05-05 20:29:41 +0200

    stable v

--- bot.py
+++ bot.py
@@ -19,31 +19,46 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True, content_types=['text', 'voice', 'audio'])
 def echo_all(message):
     if message.content_type=='voice':
-        ebird_code, pic_url, sci_name, common_name =read_audio(message, bot)
-        bot.reply_to(message,
-                     f"Alright, {message.from_user.first_name}. Here's what your recording sounds to me\n\n\nSpecies name: {sci_name}\nCommon name: [{common_name}](http://en.wikipedia.org/wiki/{re.sub(' ', '_', sci_name)})\nDuration: **{message.voice.duration}** sec"
-                     ,parse_mode='markdown')
-        img = Image.open(requests.get(pic_url, stream=True).raw)
-        bot.send_photo(message.chat.id, img)
+        try:
+            if message.voice.duration < 5:  ## check for audio length
+                bot.reply_to(message,
+                             f"Hey, {message.from_user.first_name}. I need a sample larger than 5 seconds, could you send me another one?"
+                             , parse_mode='markdown')
+            else:
+                bird_proba, pic_url, sci_name, common_name =read_audio(message, bot)
+                bot.reply_to(message,
+                             f"Alright, {message.from_user.first_name}. Your recording is **{message.voice.duration}** sec long. Here's what it sounds to me\n\n\nSpecies: {sci_name}\nCommon name: [{common_name}](http://en.wikipedia.org/wiki/{re.sub(' ', '_', sci_name)})"
+                             ,parse_mode='markdown')
+                #img = Image.open(requests.get(pic_url, stream=True).raw)
+                #bot.send_photo(message.chat.id, img)
+        except Exception as e:
+            bot.reply_to(message,
+                         f"Hey, {message.from_user.first_name}. I am sorry, but something went wrong and i cannot process your recording for whatever reason. My maker has been notified \n "
+                         , parse_mode='markdown')
+            print(f"message.voice issue: {e}")
 
     elif message.content_type=='audio':
         try:
-            if message.audio.duration < 181: ## check for audio length
-                ebird_code, pic_url, sci_name, common_name = read_audio(message, bot, message_type='audio')
+            if message.audio.duration < 5: ## check for audio length
                 bot.reply_to(message,
-                             f"Got your audio, {message.from_user.first_name}. Here's what it sounds to me\n\n\nSpecies name: {sci_name}\nCommon name: [{common_name}](http://en.wikipedia.org/wiki/{re.sub(' ', '_', sci_name)})\nDuration: **{message.audio.duration}** sec"
-                             ,parse_mode='markdown')
-                img = Image.open(requests.get(pic_url, stream=True).raw)
-                bot.send_photo(message.chat.id, img)
-            else:
+                             f"Hey, {message.from_user.first_name}. I need a sample larger than 5 seconds, could you send me another one?"
+                             , parse_mode='markdown')
+            elif message.audio.duration >180:
                 bot.reply_to(message,
                              f"Hey, {message.from_user.first_name}. I am sorry, but your audio file exceeds 3 minute limit. Please send me a shorter file"
                              , parse_mode='markdown')
+            else:
+                ebird_code, pic_url, sci_name, common_name = read_audio(message, bot, message_type='audio')
+                bot.reply_to(message,
+                             f"Got your audio, {message.from_user.first_name}. It is **{message.voice.duration}** sec long. Here's what it sounds to me\n\n\nSpecies (Top Guess): {sci_name}\nCommon name: [{common_name}](http://en.wikipedia.org/wiki/{re.sub(' ', '_', sci_name)})"
+                             ,parse_mode='markdown')
+                #img = Image.open(requests.get(pic_url, stream=True).raw)
+                #bot.send_photo(message.chat.id, img)
         except Exception as e:
             bot.reply_to(message,
-                         f"Hey, {message.from_user.first_name}. I am sorry, but something went wrong and i cannot process your file for whatever reason. Notify my maker. tell him that \n {e}"
+                         f"Hey, {message.from_user.first_name}. I am sorry, but something went wrong and i cannot process your file for whatever reason. My maker has been notified. \n "
                          , parse_mode='markdown')
-            print('Input audio file issue')
+            print(f"message.audio issue: {e}")
     else:
         bot.reply_to(message,
                      "Thank you but I'd rather you sent me a voice recording or an audio file. Try again!")
